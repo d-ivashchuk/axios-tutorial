443cd2260680029f2c28681dee4f4c17940eeb9d
DmytroI <dmytroi@MacBook-Pro-2.local>
2018-06-26 22:30:03 +0200

    add dummy axios requests

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,13 +10,25 @@ class FullPost extends Component {
 
   componentDidUpdate() {
     if (this.props.id) {
-      axios
-        .get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
-        .then(response => {
-          this.setState({ loadedPost: response.data });
-        });
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios
+          .get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+          .then(response => {
+            this.setState({ loadedPost: response.data });
+          });
+      }
     }
   }
+  deletePostHandler = () => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+      .then(response => {
+        console.log(response);
+      });
+  };
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
     if (this.props.id) {
@@ -28,7 +40,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button onClick={this.deletePostHandler} className="Delete">
+              Delete
+            </button>
           </div>
         </div>
       );
